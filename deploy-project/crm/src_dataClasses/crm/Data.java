d64a2422aac6cff29a07772e1238d8509ad530b1
als <alexander.suter@axonivy.com>
2017-02-27 15:54:58 +0100

    Add sample for single ivy project deployment

---
+++ deploy-project/crm/src_dataClasses/crm/Data.java
@@ -0,0 +1,32 @@
+package crm;
+
+/**
+ */
+@SuppressWarnings("all")
+@javax.annotation.Generated(comments="This is the java file of the ivy data class Data", value={"ch.ivyteam.ivy.scripting.streamInOut.IvyScriptJavaClassBuilder"})
+public class Data extends ch.ivyteam.ivy.scripting.objects.CompositeObject
+{
+  /** SerialVersionUID */
+  private static final long serialVersionUID = 1663954779980295582L;
+
+  private transient crm.Customer newCustomer;
+
+  /**
+   * Gets the field newCustomer.
+   * @return the value of the field newCustomer; may be null.
+   */
+  public crm.Customer getNewCustomer()
+  {
+    return newCustomer;
+  }
+
+  /**
+   * Sets the field newCustomer.
+   * @param _newCustomer the new value of the field newCustomer.
+   */
+  public void setNewCustomer(crm.Customer _newCustomer)
+  {
+    newCustomer = _newCustomer;
+  }
+
+}
