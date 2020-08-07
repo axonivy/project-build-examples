cbf70242aaf224d53c642bcc51c0ba3575b188a9
Reguel Wermelinger <reguel.wermelinger@axonivy.com>
2020-08-07 13:04:05 +0200

    XIVY-4165 simple JPA example

---
+++ compile-test/crmTests/src_test/ch/ivyteam/test/TestCustomerRepo.java
@@ -0,0 +1,36 @@
+package ch.ivyteam.test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import ch.ivyteam.crm.CustomerRepo;
+import ch.ivyteam.ivy.environment.IvyTest;
+import crm.Customer;
+
+@IvyTest
+public class TestCustomerRepo {
+
+	@Test
+	public void findCustomerByZip()
+	{
+		CustomerRepo.persist(newCustomer("Caty", "6300"));
+		CustomerRepo.persist(newCustomer("Reguel", "6210"));
+		CustomerRepo.persist(newCustomer("Reto", "6403"));
+		
+		List<Customer> citizensOfSursee = CustomerRepo.findByZip("6210");
+		assertThat(citizensOfSursee).hasSize(1);
+		assertThat(citizensOfSursee.get(0).getFirstname()).isEqualTo("Reguel");
+	}
+
+	private static Customer newCustomer(String name, String zip) 
+	{
+		Customer customer = new Customer();
+		customer.setFirstname(name);
+		customer.setZip(zip);
+		return customer;
+	}
+	
+}
