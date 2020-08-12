139572de998a7f946c4884343421c3b620bd0089
Reguel Wermelinger <reguel.wermelinger@axonivy.com>
2020-08-12 15:27:35 +0200

    XIVY-4165 in memory test-env for JPA example
    - and ivy-whitespace formatting

--- compile-test/crmTests/src_test/ch/ivyteam/test/TestCustomerRepo.java
+++ compile-test/crmTests/src_test/ch/ivyteam/test/TestCustomerRepo.java
@@ -7,30 +7,32 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 
 import ch.ivyteam.crm.CustomerRepo;
+import ch.ivyteam.ivy.environment.AppFixture;
 import ch.ivyteam.ivy.environment.IvyTest;
 import crm.Customer;
 
 @IvyTest
-public class TestCustomerRepo {
+public class TestCustomerRepo
+{
+  @Test
+  public void findCustomerByZip(AppFixture fixture)
+  {
+    fixture.environment("test-env");
 
-	@Test
-	public void findCustomerByZip()
-	{
-		CustomerRepo.persist(newCustomer("Caty", "6300"));
-		CustomerRepo.persist(newCustomer("Reguel", "6210"));
-		CustomerRepo.persist(newCustomer("Reto", "6403"));
-		
-		List<Customer> citizensOfSursee = CustomerRepo.findByZip("6210");
-		assertThat(citizensOfSursee).hasSize(1);
-		assertThat(citizensOfSursee.get(0).getFirstname()).isEqualTo("Reguel");
-	}
+    CustomerRepo.persist(newCustomer("Caty", "6300"));
+    CustomerRepo.persist(newCustomer("Reguel", "6210"));
+    CustomerRepo.persist(newCustomer("Reto", "6403"));
 
-	private static Customer newCustomer(String name, String zip) 
-	{
-		Customer customer = new Customer();
-		customer.setFirstname(name);
-		customer.setZip(zip);
-		return customer;
-	}
-	
+    List<Customer> citizensOfSursee = CustomerRepo.findByZip("6210");
+    assertThat(citizensOfSursee).hasSize(1);
+    assertThat(citizensOfSursee.get(0).getFirstname()).isEqualTo("Reguel");
+  }
+
+  private static Customer newCustomer(String name, String zip)
+  {
+    Customer customer = new Customer();
+    customer.setFirstname(name);
+    customer.setZip(zip);
+    return customer;
+  }
 }
