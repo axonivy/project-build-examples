57d539721a8a0532713245149bef0a73bd8a1c66
Lukas Lieb <lukas.lieb@axonivy.com>
2021-02-03 14:20:12 +0100

    fix raise script

--- raise-build-plugin-version.sh
+++ raise-build-plugin-version.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 mvn --batch-mode versions:set-property versions:commit -Dproperty=project.build.plugin.version -DnewVersion=${1} -DprocessAllModules -DallowSnapshots=true
+mvn --batch-mode -f deploy/application/maven/config/pom.xml versions:set-property versions:commit -Dproperty=project.build.plugin.version -DnewVersion=${1} -DprocessAllModules -DallowSnapshots=true
