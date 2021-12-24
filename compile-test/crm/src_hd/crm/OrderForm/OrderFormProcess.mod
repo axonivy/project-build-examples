ce3ceda8fe605801b3a87a0c506bec8e288fd797
Reto Weiss <reto.weiss@axonivy.com>
2021-12-24 11:23:41 +0100

    Cleanup 9.3 branch (pom.versions, plugin version, convert projects)
    Streamline all pom.xml versions to 9.3.0-SNAPSHOT
    Use project build plugin version 9.3.1
    Convert projects to latest 9.3 version

--- compile-test/crm/src_hd/crm/OrderForm/OrderFormProcess.mod
+++ compile-test/crm/src_hd/crm/OrderForm/OrderFormProcess.mod
@@ -1,5 +1,5 @@
 [Ivy]
-1700F6E185CAFF13 9.3.0 #module
+1700F6E185CAFF13 9.3.1 #module
 >Proto >Proto Collection #zClass
 Os0 OrderFormProcess Big #zClass
 Os0 RD #cInfo
--- raise-build-plugin-version.sh
+++ raise-build-plugin-version.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
 mvn --batch-mode versions:set-property versions:commit -Dproperty=project.build.plugin.version -DnewVersion=${1} -DprocessAllModules -DallowSnapshots=true
-mvn --batch-mode -f deploy/application/maven/config/pom.xml versions:set-property versions:commit -Dproperty=project.build.plugin.version -DnewVersion=${1} -DprocessAllModules -DallowSnapshots=true
