3b47aeb6da588e544e6b30fe11d0b38980774ecd
Louis <louis.mueller@axonivy.com>
2025-06-16 10:23:50 +0200

    do feedback: dedicated raise module

--- .ivy/raise-version.sh
+++ .ivy/raise-version.sh
@@ -4,7 +4,7 @@ set -e
 # Updates all example artifact versions in all pom.xml files
 
 # Update artifact versions in reactor POM and all modules POMs 
-mvn --batch-mode versions:set versions:commit -DnewVersion=${1} -DprocessAllModules
+mvn --batch-mode -f maven/pom.xml versions:set versions:commit -DnewVersion=${1} -DprocessAllModules
 
 # Update artifact version in Dockerfile
 sed -i -b -E  "s:(ivy-application-)([0-9\.]+-SNAPSHOT)(\.zip):\1${1}\3:g" docker/image/Dockerfile
